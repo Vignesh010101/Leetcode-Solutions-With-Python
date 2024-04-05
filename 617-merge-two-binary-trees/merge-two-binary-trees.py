61025c66657872541a64da1ada054a9badb69a9c
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-04-05 11:39:48 +0530

    Time: 59 ms (74.85%) | Memory: 16.9 MB (93.34%) - LeetSync

--- 617-merge-two-binary-trees/merge-two-binary-trees.py
+++ 617-merge-two-binary-trees/merge-two-binary-trees.py
@@ -4,14 +4,33 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
-  def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
-    if not root1 and not root2:
-      return None
-    val = (root1.val if root1 else 0) + (root2.val if root2 else 0)
-    root = TreeNode(val)
-    root.left = self.mergeTrees(root1.left if root1 else None,
-                                root2.left if root2 else None)
-    root.right = self.mergeTrees(root1.right if root1 else None,
-                                 root2.right if root2 else None)
-    return root   
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+
+        stack1 = [root1]
+        stack2 = [root2]
+        if not root1:
+            return root2
+
+        while stack1:
+            n1 = stack1.pop()
+            n2 = stack2.pop()
+
+            if n1 and n2:
+                n1.val += n2.val
+
+                if n2.left and not n1.left:
+                    n1.left = TreeNode()
+                if n2.right and not n1.right:
+                    n1.right = TreeNode()
+
+                stack1.append(n1.left)
+                stack1.append(n1.right)
+                stack2.append(n2.left)
+                stack2.append(n2.right)
+
+        return root1
+
+
+                
