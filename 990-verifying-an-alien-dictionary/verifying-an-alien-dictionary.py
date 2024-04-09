9f6b289d2d38ae5fc0af4ab47ec027a1d21afd44
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-04-09 23:22:50 +0530

    Time: 43 ms (37.07%) | Memory: 16.5 MB (76.83%) - LeetSync

--- 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.py
+++ 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.py
@@ -1,5 +1,21 @@
 class Solution:
-  def isAlienSorted(self, words: List[str], order: str) -> bool:
-    dict = {c: i for i, c in enumerate(order)}
-    words = [[dict[c] for c in word] for word in words]
-    return all(w1 <= w2 for w1, w2 in zip(words, words[1:]))
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        mapper = {chr(i):-1 for i in range(97,97+26)}
+        def map_value():
+            nonlocal order
+            nonlocal mapper
+            for i in range(len(order)):
+                mapper[order[i]] = i
+        map_value()
+        def helper():
+            for i in range(len(words)-1):
+                if len(words[i+1])<len(words[i]) and mapper[words[i+1][0]] <= mapper[words[i][0]] and words[i+1] in words[i]:
+                    return False
+                else:
+                    for j in range(min(len(words[i]), len(words[i+1]))):
+                        if words[i][j] != words[i+1][j]:
+                            if mapper[words[i+1][j]] < mapper[words[i][j]]:
+                                return False
+                            break
+            return True
+        return helper()
