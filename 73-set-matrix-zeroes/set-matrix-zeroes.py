35af370446fa3c9a6ac56a699bb98b789e61b78b
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-04-22 08:27:51 +0530

    Time: 106 ms (38.81%) | Memory: 17.6 MB (28.20%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,28 +1,18 @@
 class Solution:
-  def setZeroes(self, matrix: List[List[int]]) -> None:
-    m = len(matrix)
-    n = len(matrix[0])
-    shouldFillFirstRow = 0 in matrix[0]
-    shouldFillFirstCol = 0 in list(zip(*matrix))[0]
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n, m = len(matrix), len(matrix[0])
+        row = [0] * n
+        col = [0] * m
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j] == 0:
+                    row[i] = 1
+                    col[j] = 1
 
-    # Store the information in the first row and the first column.
-    for i in range(1, m):
-      for j in range(1, n):
-        if matrix[i][j] == 0:
-          matrix[i][0] = 0
-          matrix[0][j] = 0
-
-    # Fill 0s for the matrix except the first row and the first column.
-    for i in range(1, m):
-      for j in range(1, n):
-        if matrix[i][0] == 0 or matrix[0][j] == 0:
-          matrix[i][j] = 0
-
-    # Fill 0s for the first row if needed.
-    if shouldFillFirstRow:
-      matrix[0] = [0] * n
-
-    # Fill 0s for the first column if needed.
-    if shouldFillFirstCol:
-      for row in matrix:
-        row[0] = 0
+        for i in range(n):
+            for j in range(m):
+                if row[i] or col[j]:
+                    matrix[i][j] = 0
