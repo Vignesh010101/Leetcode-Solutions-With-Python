09da792c9af3a33660c71d286d0eadde05e80936
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-04-29 22:15:31 +0530

    Time: 80 ms (67.79%) | Memory: 24.1 MB (45.66%) - LeetSync

--- 609-find-duplicate-file-in-system/find-duplicate-file-in-system.py
+++ 609-find-duplicate-file-in-system/find-duplicate-file-in-system.py
@@ -1,19 +1,10 @@
 class Solution:
-  def findDuplicate(self, paths: List[str]) -> List[List[str]]:
-    contentToPathFiles = collections.defaultdict(list)
+    def findDuplicate(self, paths: List[str]) -> List[List[str]]:
+        dir=defaultdict(list)
+        for path in paths:
+            directory,*files=path.split()
+            for file in files:
+                filename, content=file.split('(')
+                dir[content].append(directory+'/'+filename)
 
-    for path in paths:
-      words = path.split(' ')
-      rootPath = words[0]  # "root/d1/d2/.../dm"
-      for fileAndContent in words[1:]:  # "fn.txt(fn_content)"
-        l = fileAndContent.find('(')
-        r = fileAndContent.find(')')
-        # "fn.txt"
-        file = fileAndContent[:l]
-        # "fn_content"
-        content = fileAndContent[l + 1:r]
-        # "root/d1/d2/.../dm/fn.txt"
-        filePath = rootPath + '/' + file
-        contentToPathFiles[content].append(filePath)
-
-    return [filePath for filePath in contentToPathFiles.values() if len(filePath) > 1]
+        return [paths for paths in dir.values() if len(paths)>1]
