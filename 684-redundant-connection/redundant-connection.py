c91a3f3bb7336cc04e1ed84f970882dcb0595682
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-05-02 22:55:32 +0530

    Time: 54 ms (64.37%) | Memory: 17 MB (53.96%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -1,35 +1,33 @@
 class UnionFind:
-    def __init__(self,n):
-        self.par = [i for i in range(n+1)]
-        self.rank = [1]*(n+1)
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.size = [1] * n
 
-    def find(self,x):
-        while x!=self.par[x]:
-            self.par[x] = self.par[self.par[x]]
-            x = self.par[x]
-        
-        return x
+    def find(self, i):
+        if self.parent[i] != i:
+            self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
+    
+    def UnionBySize(self, irep, jrep):
+        isize = self.size[irep]
+        jsize = self.size[jrep]
 
-    def union(self,n1,n2):
-        p1,p2 = self.find(n1),self.find(n2)
-        if p1==p2:
-            return False
-        
-        if self.rank[p1]<self.rank[p2]:
-            self.rank[p2]+=self.rank[p1]
-            self.par[p1]=self.par[p2]
+        if isize < jsize:
+            self.parent[irep] = jrep
+            self.size[jrep] += self.size[irep]
         else:
-            self.rank[p1]+=self.rank[p2]
-            self.par[p2]=self.par[p1]
-
-        return True
+            self.parent[jrep] = irep
+            self.size[irep] += self.size[jrep]
 
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        ans = 0
-        uf = UnionFind(len(edges))
-
-        for a,b in edges:
-            if not uf.union(a,b):
-                return [a,b]
-        
+        n = len(edges)
+        uf = UnionFind(n)
+        for (i, j) in edges:
+            irep = uf.find(i-1)
+            jrep = uf.find(j-1)
+            if irep != jrep:
+                uf.UnionBySize(irep, jrep)
+            else:
+                return [i, j]
+            
