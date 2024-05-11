83ea34126aaf3ef70b4f05c744546cbadd399412
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-05-11 20:29:20 +0530

    Time: 157 ms (50.74%) | Memory: 19.1 MB (22.85%) - LeetSync

--- 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.py
+++ 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.py
@@ -1,17 +1,23 @@
 class Solution:
-  def mincostToHireWorkers(self, quality: List[int], wage: List[int], k: int) -> float:
-    ans = math.inf
-    qualitySum = 0
-    # (wagePerQuality, quality) sorted by wagePerQuality
-    workers = sorted((w / q, q) for q, w in zip(quality, wage))
-    maxHeap = []
-
-    for wagePerQuality, q in workers:
-      heapq.heappush(maxHeap, -q)
-      qualitySum += q
-      if len(maxHeap) > k:
-        qualitySum += heapq.heappop(maxHeap)
-      if len(maxHeap) == k:
-        ans = min(ans, qualitySum * wagePerQuality)
-
-    return ans
+    def mincostToHireWorkers(
+        self, quality: List[int], wage: List[int], k: int
+    ) -> float:
+        ratio = sorted([(w / q, q) for w, q in zip(wage, quality)])
+        max_heap = []
+        quality_sum = 0
+        max_ratio = 0.0
+        
+        for i in range(k):
+            max_ratio = max(max_ratio, ratio[i][0])
+            quality_sum += ratio[i][1]
+            heapq.heappush(max_heap, -ratio[i][1])
+        
+        res = max_ratio * quality_sum
+        
+        for i in range(k, len(quality)):
+            max_ratio = max(max_ratio, ratio[i][0])
+            quality_sum += ratio[i][1] + heapq.heappop(max_heap)
+            heapq.heappush(max_heap, -ratio[i][1])
+            res = min(res, max_ratio * quality_sum)
+        
+        return res
