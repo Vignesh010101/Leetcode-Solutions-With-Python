bd9a6baeb7dc5d0fe027c0e418fe6c36f48054d5
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-05-19 12:47:58 +0530

    Time: 119 ms (86.03%) | Memory: 17.5 MB (56.00%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -1,15 +1,3 @@
 class Solution:
     def minFallingPathSum(self, m: List[List[int]]) -> int:
-        n = len(m)
-
-        @cache
-        def f(i, j):
-            if 0 <= j < n:
-                if i < n:
-                    return m[i][j] + min(f(i+1,j-1), f(i+1,j), f(i+1,j+1))
-                else:
-                    return 0
-            else:
-                return inf
-
-        return min(f(0,j) for j in range(n))
+        return min(reduce(lambda a,r:[q+min(a[max(0,j-1):j+2]) for j,q in enumerate(r)],m))
