880f90f793f1effabb35635b50a524b7cc03b2fb
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-05-22 18:36:23 +0530

    Time: 1793 ms (17.28%) | Memory: 68.7 MB (29.64%) - LeetSync

--- 2213-find-all-people-with-secret/find-all-people-with-secret.py
+++ 2213-find-all-people-with-secret/find-all-people-with-secret.py
@@ -1,51 +1,27 @@
-class UnionFind:
-  def __init__(self, n: int):
-    self.id = list(range(n))
-    self.rank = [0] * n
+class Solution: #Dijkstra's Algorithm
+    def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
+        graph = defaultdict(list)
 
-  def unionByRank(self, u: int, v: int) -> None:
-    i = self._find(u)
-    j = self._find(v)
-    if i == j:
-      return
-    if self.rank[i] < self.rank[j]:
-      self.id[i] = j
-    elif self.rank[i] > self.rank[j]:
-      self.id[j] = i
-    else:
-      self.id[i] = j
-      self.rank[j] += 1
+        for x,y,t in meetings:
+            graph[x].append((y,t))
+            graph[y].append((x,t))
 
-  def connected(self, u: int, v: int) -> bool:
-    return self._find(self.id[u]) == self._find(self.id[v])
+        heap = [(0,firstPerson)] #time,person
+        for nei,time in graph[0]:
+            heapq.heappush(heap,(time,nei))
+        
+        visited = set([0])
 
-  def reset(self, u: int) -> None:
-    self.id[u] = u
+        while heap:
+            time,person = heapq.heappop(heap)
 
-  def _find(self, u: int) -> int:
-    if self.id[u] != u:
-      self.id[u] = self._find(self.id[u])
-    return self.id[u]
+            if person in visited:
+                continue
 
+            visited.add(person)
 
-class Solution:
-  def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
-    uf = UnionFind(n)
-    timeToPairs = collections.defaultdict(list)
+            for nei,t in graph[person]:
+                if t>=time:
+                    heapq.heappush(heap,(t,nei))
 
-    uf.unionByRank(0, firstPerson)
-
-    for x, y, time in meetings:
-      timeToPairs[time].append((x, y))
-
-    for _, pairs in sorted(timeToPairs.items(), key=lambda x: x[0]):
-      peopleUnioned = set()
-      for x, y in pairs:
-        uf.unionByRank(x, y)
-        peopleUnioned.add(x)
-        peopleUnioned.add(y)
-      for person in peopleUnioned:
-        if not uf.connected(person, 0):
-          uf.reset(person)
-
-    return [i for i in range(n) if uf.connected(i, 0)]
+        return visited
