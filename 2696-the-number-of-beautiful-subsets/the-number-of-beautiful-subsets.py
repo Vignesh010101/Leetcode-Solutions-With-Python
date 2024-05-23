69dcffd317b15da973da097cc4ea9a5296585e66
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-05-23 23:54:00 +0530

    Time: 3110 ms (38.96%) | Memory: 16.5 MB (84.32%) - LeetSync

--- 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.py
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.py
@@ -1,4 +1,32 @@
 class Solution:
     def beautifulSubsets(self, nums: List[int], k: int) -> int:
-        def backtrack(i, cur): return (1 if cur else 0) + sum(backtrack(i+1, cur | {nums[i]}) for i in range(i, len(nums)) if all(abs(nums[i] - x) != k for x in cur))
-        return backtrack(0, set())
+        # nums = [2,4,6]
+        # k = 2
+        # [], [2], [4], [6], [2, 4], [2, 6], [4, 6], [2, 4, 6]
+        # [2], [4], [6], [2, 6] -> 4 ans
+        # SOLUTION
+        # num - k or n + k
+
+        count = 0
+        lenNums = len(nums)
+
+        def explore(index):
+            nonlocal count
+            if lenNums == index:
+                count += 1
+                return
+
+            num = nums[index]
+
+            if num - k not in visited and num + k not in visited:
+                visited[num] += 1
+                explore(index + 1)
+                visited[num] -= 1
+                if visited[num] == 0:
+                    del visited[num]
+
+            explore(index + 1)
+
+        visited = defaultdict(int)
+        explore(0)
+        return count - 1
