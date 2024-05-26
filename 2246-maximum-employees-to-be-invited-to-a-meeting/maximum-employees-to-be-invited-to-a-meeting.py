c0bfe976a234ff1ad1f73e97942fcea2c4575e63
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-05-26 15:21:26 +0530

    Time: 907 ms (50.78%) | Memory: 74.1 MB (25.00%) - LeetSync

--- 2246-maximum-employees-to-be-invited-to-a-meeting/maximum-employees-to-be-invited-to-a-meeting.py
+++ 2246-maximum-employees-to-be-invited-to-a-meeting/maximum-employees-to-be-invited-to-a-meeting.py
@@ -1,42 +1,45 @@
 class Solution:
     def maximumInvitations(self, favorite: List[int]) -> int:
+        ans = 0
         n = len(favorite)
-        graph = [[] for _ in range(n)]
-        for i, x in enumerate(favorite): graph[x].append(i)
+        cycles_of_2 = []
+        seen = [False for _ in range(n)]
+        for node in range(n):
+            if seen[node]:
+                continue
+            if favorite[favorite[node]] == node:
+                cycles_of_2.append((node, favorite[node]))
+                seen[node] = True
+                seen[favorite[node]] = True
+        for node in range(n):
+            if seen[node]:
+                continue
+            cycle_length = self.find_cycle(node, 1, {}, seen, favorite)
+            ans = max(ans, cycle_length)
         
-        def bfs(x, seen): 
-            """Return longest arm of x."""
-            ans = 0 
-            queue = deque([x])
-            while queue: 
-                for _ in range(len(queue)): 
-                    u = queue.popleft()
-                    for v in graph[u]: 
-                        if v not in seen: 
-                            seen.add(v)
-                            queue.append(v)
-                ans += 1
-            return ans 
-        
-        ans = 0 
-        seen = [False]*n
-        for i, x in enumerate(favorite): 
-            if favorite[x] == i and not seen[i]: 
-                seen[i] = seen[x] = True 
-                ans += bfs(i, {i, x}) + bfs(x, {i, x})
-                
-        dp = [0]*n
-        for i, x in enumerate(favorite): 
-            if dp[i] == 0: 
-                ii, val = i, 0
-                memo = {}
-                while ii not in memo: 
-                    if dp[ii]: 
-                        cycle = dp[ii]
-                        break
-                    memo[ii] = val
-                    val += 1
-                    ii = favorite[ii]
-                else: cycle = val - memo[ii]
-                for k in memo: dp[k] = cycle
-        return max(ans, max(dp))
+        if len(cycles_of_2) != 0:
+            revfavorite = defaultdict(list)
+            for node, fav in enumerate(favorite):
+                if favorite[fav] != node:
+                    revfavorite[fav].append(node)
+            total_tail = 0
+            for x, y in cycles_of_2:
+                max_tail = self.max_depth(x, revfavorite) + self.max_depth(y, revfavorite)
+                total_tail += max_tail
+            ans = max(ans, 2 * len(cycles_of_2) + total_tail)
+        return ans
+
+    def find_cycle(self, node, length, path, seen, favorite):
+        if seen[node]:
+            return -1
+        seen[node] = True
+        path[node] = length
+        if favorite[node] in path:
+            return path[node] - path[favorite[node]] + 1
+        return self.find_cycle(favorite[node], length+1, path, seen, favorite)
+    
+    def max_depth(self, node, graph, depth=0):
+        res = depth
+        for neigh in graph[node]:
+            res = max(res, self.max_depth(neigh, graph, depth+1))
+        return res
