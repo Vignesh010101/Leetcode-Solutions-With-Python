4e62c8115ffdfa996c13cd2a85b6a21eb42cc2d9
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-05-31 16:11:16 +0530

    Time: 1264 ms (52.87%) | Memory: 30.4 MB (55.17%) - LeetSync

---
+++ 2301-count-array-pairs-divisible-by-k/count-array-pairs-divisible-by-k.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countPairs(self, nums: List[int], k: int) -> int:
+        factors = []
+        for x in range(1, int(sqrt(k))+1):
+            if k % x == 0: factors.append(x)
+        ans = 0 
+        freq = Counter()
+        for x in nums: 
+            x = gcd(x, k)
+            ans += freq[k//x]
+            for f in factors: 
+                if x % f == 0 and f <= x//f: 
+                    freq[f] += 1
+                    if f < x//f: freq[x//f] += 1
+        return ans 
